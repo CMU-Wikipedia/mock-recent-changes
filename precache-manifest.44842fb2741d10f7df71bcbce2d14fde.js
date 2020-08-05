08131e457c0a239f4df4adb6879a37b55c2e5c9e
ShaurGaur <shauryavrat@live.com>
2020-08-05 13:54:08 -0700

    Updates

---
+++ precache-manifest.44842fb2741d10f7df71bcbce2d14fde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9e7dabee937b889e202a8b8a75e857",
+    "url": "/mock-recent-changes/index.html"
+  },
+  {
+    "revision": "39004b8b8693c4ff90ef",
+    "url": "/mock-recent-changes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e51244689dc73c2fb5a",
+    "url": "/mock-recent-changes/static/js/2.37f6a2f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mock-recent-changes/static/js/2.37f6a2f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39004b8b8693c4ff90ef",
+    "url": "/mock-recent-changes/static/js/main.9f29ac57.chunk.js"
+  },
+  {
+    "revision": "5bf0f6acebb86ca2a9c0",
+    "url": "/mock-recent-changes/static/js/runtime-main.776d3229.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mock-recent-changes/static/media/logo.5d5d9eef.svg"
+  }
+]);
